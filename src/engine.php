183a5b09dcb38168b404dd6d66a84921d57412d5
Alexandr <airmanjke57@gmail.com>
2023-04-24 02:49:51 +0200

    eve game 1.0

---
+++ src/engine.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Games\engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function Logic(array $arra, array $arrq, $explanation)
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line($explanation);
+    $score = 0;
+    for($i = 0; $score <= 3; $i++){
+        line("Question: $arrq[$i]");
+        $answer = prompt("Your answer");
+        if ($answer == $arra[$i]){
+            line('Correct!');
+        $score++;  
+        }else {
+            line("'$answer' is wrong answer;(. Correct answer '$arra[$i]'\n Let's try again, $name");
+        return;
+        }if ($score == 3){
+            line("Congratulations, %s!", $name);
+        return;
+        }
+}
+}
--- src/game.php
+++ src/game.php
@@ -2,34 +2,19 @@
 
 namespace Games\even;
 
-use function cli\line;
-use function cli\prompt;
+use function Games\engine\Logic;
 
 function start()
 {
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
-line('Answer "yes" if the number is even, otherwise answer "no".');
-$score = 0;
-for( ; $score <=3; ){
-$randomnumber = rand(1,100);
-$ournumber = $randomnumber;
-line("Question: $ournumber");
-$answer = prompt("Your answer");
-$correctAnswer = $ournumber % 2 == 0 ? "'yes'" : "'no'";
-    if ($ournumber % 2 == 0 && $answer == 'yes'){
-        line('Correct!');
-        $score++;      
-    }elseif ($ournumber % 2 == 1 && $answer == 'no'){
-        line('Correct!');
-        $score++;
-    }else {
-        line("'$answer' is wrong answer;(. Correct answer $correctAnswer\n Let's try again, $name");
-        return;
-    }if ($score == 3){
-        line("Congratulations, %s!", $name);
-        return;
+    $explanation = 'Answer "yes" if the number is even, otherwise answer "no".';
+    $arrq = [];
+    $arra = [];
+    for ($i = 0; $i < 3; $i++){
+        $randomnumber = rand(1,100);
+        $correctAnswer = ($randomnumber % 2 == 0) ? 'yes' : 'no';
+        $arrq[] = $randomnumber;
+        $arra[] = $correctAnswer;
+        }
+        Logic($arra, $arrq, $explanation);
     }
-}
-}
+
