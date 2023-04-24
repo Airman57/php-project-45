2311703574662bee73b6c0c3d2faf285ca8bab34
Alexandr <airmanjke57@gmail.com>
2023-04-24 19:08:23 +0200

    brain-calc 1.0

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Airman57/php-project-45/workflows/hexlet-check/badge.svg)](https://github.com/Airman57/php-project-45/actions)
  <a href="https://codeclimate.com/github/Airman57/php-project-45/maintainability"><img src="https://api.codeclimate.com/v1/badges/47b14110f4bce1cd6088/maintainability" /></a>
- Demonstration
- 1. Brain-Even
- https://asciinema.org/a/42JjThlcrGTSQd1fOeTfTd5Db
+ <a href="https://asciinema.org/a/42JjThlcrGTSQd1fOeTfTd5Db" target="_blank"><img src="https://asciinema.org/a/42JjThlcrGTSQd1fOeTfTd5Db.svg" /></a>
+ <a href="https://asciinema.org/a/RwikagFyeOV0uNAOJw6o5BapN" target="_blank"><img src="https://asciinema.org/a/RwikagFyeOV0uNAOJw6o5BapN.svg" /></a>
---
+++ src/Games/calc-game.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Games\Calc;
+
+use function Games\engine\Logic;
+
+function start()
+{
+    $explanation = 'What is the result of the expression?';
+    $arrq = [];
+    $arra = [];
+    for ($i = 0; $i < 3; $i++){
+        $operations = ['*', '-', '+'];
+        $number = mt_rand(0,2);
+        $operation = $operations[$number];
+        $a = rand(1, 25);
+        $b = rand(1, 25);
+        $question = "$a $operation $b";
+        if ($operation == "-"){
+            $correctanswer = $a - $b;
+          }elseif ($operation == "+"){
+            $correctanswer = $a + $b;
+          }elseif ($operation == "*"){
+            $correctanswer = $a * $b;
+          }
+                $arrq[] = $question;
+                $arra[] = $correctanswer;
+    }
+    Logic($arra, $arrq, $explanation);
+}
