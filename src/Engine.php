9839fe2f5e5fdebd846522b98b302e29854284bd
Alexandr <airmanjke57@gmail.com>
2023-05-02 03:52:36 +0200

    greetin funct

--- src/Engine.php
+++ src/Engine.php
@@ -4,12 +4,11 @@ namespace Games\Engine;
 
 use function cli\line;
 use function cli\prompt;
+use function BrainGames\Cli\greeting;
 
 function logic(array $gameData, string $explanation)
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    greeting();
     line($explanation);
     for ($i = 0; $i < 3; $i++) {
         $question = "Question: " . $gameData[$i]['question'];
